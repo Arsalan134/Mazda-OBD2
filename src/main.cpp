638c2f92b54a5af18b06b50e4ca1975f6f13b700
Arsalan <airavani2018@gmail.com>
2023-08-19 00:00:11 +0200

    asd

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,34 @@
 #include "main.h"
 
 void setup() {
-  // Serial.begin(115200);
+  Serial.begin(9600);
+
+  while (!Serial)
+    ;
+
+  Serial.println(F("OBD2 Supported PIDs"));
+
+  while (true) {
+    Serial.print(F("Attempting to connect to OBD2 CAN bus ... "));
+
+    if (!OBD2.begin()) {
+      Serial.println(F("failed!"));
+
+      delay(1000);
+    } else {
+      Serial.println(F("success"));
+      break;
+    }
+  }
+
+  Serial.println();
+
+  // loop through PIDs 0 to 95, reading and printing the names of the supported PIDs
+  for (int pid = 0; pid < 96; pid++) {
+    if (OBD2.pidSupported(pid)) {
+      Serial.println(OBD2.pidName(pid));
+    }
+  }
 
   while (!OBD2.begin())
     delay(200);
